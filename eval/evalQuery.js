c08c3132bf7dcf6f810c8a604dfbcbd8384dc71f
Alexander Ding <alexander.jf.ding@gmail.com>
2024-04-26 13:29:53 -0400

    feat: add query eval

--- eval/evalQuery.js
+++ eval/evalQuery.js
@@ -1,19 +1,32 @@
 const { query } = require("./../bin/query");
 const { start, promisify } = require("./../bin/lib");
+const fs = require("fs");
 const distribution = require("./../distribution");
 const args = require("yargs").argv;
 
-const word = "function";
-
 const evalQuery = () => {
-  const t0 = performance.now();
-  promisify(query)(word, 5)
-    .then((res) => {
-      const t1 = performance.now();
-      console.log(`query time: ${t1 - t0}`);
-      console.log(res);
+  const words = fs
+    .readFileSync("data/5000-more-common.txt", "utf-8")
+    .split("\n");
+  const allStart = performance.now();
+  Promise.all(
+    words.map((word) => {
+      const start = performance.now();
+      return promisify(query)(word, 5)
+        .then((res) => {
+          const end = performance.now();
+          const time = end - start;
+          // console.log(time);
+          return time;
+        })
+        .catch(console.error);
     })
-    .catch(console.error);
+  ).then((times) => {
+    const allEnd = performance.now();
+    console.log("Total time:", allEnd - allStart);
+    console.log("Throughput:", words.length / ((allEnd - allStart) / 1000));
+    // console.log(times.reduce((a, b) => a + b) / times.length);
+  });
 };
 
 start(args.nodesConfig || "data/nodesConfig.json", evalQuery);
